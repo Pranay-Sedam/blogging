344b5300446bc4e2e8f4727938aa00ec5a4026c8
Pranay Sedam <pranaysedam3@gmail.com>
2024-09-18 12:05:07 +0530

    "Home page skelton added "

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <a href="" className='logo'>MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+        <div className="post">
+          <div className="image">
+          <img src="https://www.tesmanian.com/cdn/shop/articles/Model3_77_RHD_04d1427f-e398-4129-aea6-8af8eb9d485c.jpg?v=1698864089&width=1000" alt="" />
+          </div>
+          
+          <div className="text">
+          <h2>Tesla is set to be part of the Treasury and IRS Clean Vehicle Tax Credit Portal for Car Dealers.</h2>
+            <p className="info">
+              <a href="" className="author">Prince Leo</a>
+              <time>2024-1-1 10:10</time>
+            </p>
+            <p className='summary'>
+            The U.S. Department of the Treasury and the IRS have initiated the registration process for car dealers, 
+            including Tesla, to access the IRS Energy Credits 
+            </p>
+          </div>   
+        </div>
+        <div className="post">
+          <div className="image">
+          <img src="https://www.tesmanian.com/cdn/shop/articles/Model3_77_RHD_04d1427f-e398-4129-aea6-8af8eb9d485c.jpg?v=1698864089&width=1000" alt="" />
+          </div>
+          
+          <div className="text">
+          <h2>Tesla is set to be part of the Treasury and IRS Clean Vehicle Tax Credit Portal for Car Dealers.</h2>
+            <p className="info">
+              <a href="" className="author">Prince Leo</a>
+              <time>2024-1-1 10:10</time>
+            </p>
+            <p className='summary'>
+            The U.S. Department of the Treasury and the IRS have initiated the registration process for car dealers, 
+            including Tesla, to access the IRS Energy Credits 
+            </p>
+          </div>  
+        </div>
+        <div className="post">
+          <div className="image">
+          <img src="https://www.tesmanian.com/cdn/shop/articles/Model3_77_RHD_04d1427f-e398-4129-aea6-8af8eb9d485c.jpg?v=1698864089&width=1000" alt="" />
+          </div>
+          
+          <div className="text">
+          <h2>Tesla is set to be part of the Treasury and IRS Clean Vehicle Tax Credit Portal for Car Dealers.</h2>
+            <p className="info">
+              <a href="" className="author">Prince Leo</a>
+              <time>2024-1-1 10:10</time>
+            </p>
+            <p className='summary'>
+            The U.S. Department of the Treasury and the IRS have initiated the registration process for car dealers, 
+            including Tesla, to access the IRS Energy Credits 
+            </p>
+          </div>  
+        </div>
+    </main>
+  )
+}
+
+export default App
